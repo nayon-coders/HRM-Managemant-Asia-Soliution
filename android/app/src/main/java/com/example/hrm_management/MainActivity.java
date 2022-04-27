68d3cf171b39ce89db55a7f112e0cb89bad586cf
bond_307 <freelancer4747.com>
2022-04-27 09:42:14 +0600

    this is first

---
+++ android/app/src/main/java/com/example/hrm_management/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.hrm_management;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Utility/color.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class appColors{
+  static const Color mainColor = Color(0xff051C4B);
+  static const Color secondaryColor = Color(0xff0F5EF7);
+  static const Color titleTextColor = Color(0xff3C4858);
+  static const Color subTitleTextColor = Color(0xff8492A6);
+}
